a34e8c5f08c073c9c723f5f941ebbe7d5ef947b7
Karam-Salama <karamslama244@gmail.com>
2024-10-13 23:25:10 +0300

    just add firebase and initilaize it

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hup/firebase_options.dart';
 import 'app/fruitHup_app.dart';
 import 'core/database/cache/cache_helper.dart';
+import 'core/functions/check_state_changes_fun.dart';
 import 'core/service/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setUpServiceLocator();
   await getIt<CacheHelper>().init();
+  checkStateChanges();
   runApp(const FruitHup());
 }
